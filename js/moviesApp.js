fae3bf9c09925458f65b52bfc89f69099b7c3968
AdrianVvazquez <josea.vazquez@edu.uag.mx>
2022-02-24 12:40:30 -0600

    cambios en archivos

---
+++ js/moviesApp.js
@@ -0,0 +1,9 @@
+const btnPagar = document.getElementById("btn-proceder-pago");
+
+btnPagar.onclick = () => {
+
+    window.location.href = './metodo-pago.html';
+
+    console.log('redireccionando...');
+
+};
