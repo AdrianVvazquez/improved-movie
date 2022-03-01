767d3fad57cb900fc565d506783ab818341ff42b
AdrianVvazquez <josea.vazquez@edu.uag.mx>
2022-03-01 12:57:17 -0600

    Primer prototipo

--- js/moviesApp.js
+++
@@ -1,9 +0,0 @@
-const btnPagar = document.getElementById("btn-proceder-pago");
-
-btnPagar.onclick = () => {
-
-    window.location.href = './metodo-pago.html';
-
-    console.log('redireccionando...');
-
-};
